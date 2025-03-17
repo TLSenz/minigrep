74e24eae625d4e99b0cea75882fdfd14d11317aa
TLSenz <137443590+TLSenz@users.noreply.github.com>
2025-03-17 19:19:44 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process::exit;
+
+fn get_args() -> Vec<String>{
+    let mut input:Vec<String> = vec![];
+    if env::args().len() != 3 {
+        println!("Please Provide all Necessary cli argument: cargo run [search_string][search_path]");
+        exit(1)
+    }
+    for arguments in env::args(){
+        input.push(arguments)
+    }
+    input
+}
+
+fn display_args(search_string: &String, search_path: &String){
+    println!("Search Query: {}", search_string);
+    println!("Search Path: {}", search_path)
+}
+
+fn main() {}
