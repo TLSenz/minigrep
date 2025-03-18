bfbd622d3662915eb54688cb29e3e4b52230c645
TLSenz <sven@zemp.email>
2025-03-18 17:55:37 +0100

    update

--- src/test.rs
+++ src/test.rs
@@ -2,8 +2,15 @@
 
 
 mod tests {
+    use crate::search_file;
+
     #[test]
     fn test_search() {
-        assert!(true);
+        let  query = "duct";
+        let contents = "\
+        Rust:\
+        safe, fast, productive.\
+        Pick three.";
+        assert!(vec!["safe, fast, productive."],search_file(query,contents));
     }
 }
